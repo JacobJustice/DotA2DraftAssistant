b5a304d5bcf6eaa579cf56583bbe67c466b29c1a
Nikhil Karthikeyan <nkarthi@g.clemson.edu>
2022-01-29 18:15:15 -0500

    cleaned up variable names, added some documentation, and comparmentalized function calls

--- match_history_parser.py
+++ match_history_parser.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import json
 import os
 
-# 87655200 steam32 account ID
 pd.options.mode.chained_assignment = None  # default='warn'
+
+# Nikhil's 87655200 steam32 account ID
 
 client = opendota.OpenDota()
 
 # player = client.get_player(87655200)
+
+# client.get_player_heroes(p_id)
 
 p_id = 37571649
 
@@ -21,9 +24,10 @@ def make_dict_from_path(path):
     return json.loads(contents)
 
 def parse_match(d, p_id):
+    # Defines the dataframes that will be output 
     all_words = pd.DataFrame(d['all_word_counts'], index = ['word', 'count'])
     players = pd.DataFrame(d['players'])
-    picks = players[['hero_id', 'isRadiant', 'account_id']]
+    picks = players[['hero_id', 'isRadiant', 'account_id', 'patch']]
 
     test = players[players['account_id'] == p_id]
     
@@ -38,17 +42,23 @@ def parse_match(d, p_id):
         return None
 
 def add_to_df(with_df, against_df, pb, win, hero_stats):
+    # Defines whether the player is on the radiant or dire in this match
     player_team = pb[pb['is_player'] == True]['isRadiant'].bool()
 
+    # Sorts who is on players team and who isn't
     with_pb = pb[pb['isRadiant'] == player_team]
     against_pb = pb[pb['isRadiant'] != player_team]
 
+    # Maps player id to integer list between 0 and 121 after identifying what hero_id the player played. This step is neccessary as the hero_id does
+    # not stay within the range of 0-121
     hero_id = np.array(pb[pb['is_player'] == True]['hero_id'])[0]
     hero_id_map = int(hero_stats[hero_stats['id'] == hero_id]['idx_map'])
 
+    # Identifies hero_id for allies and enemies
     with_pb_ids = np.array(with_pb['hero_id'], dtype = int)
     against_pb_ids = np.array(against_pb['hero_id'], dtype = int)
 
+    # Maps hero_id of allies and enemies to correct for the single jump in values
     with_pb_ids_map = np.zeros(5, dtype = int)
     against_pb_ids_map = np.zeros(5, dtype = int)
 
@@ -59,49 +69,45 @@ def add_to_df(with_df, against_df, pb, win, hero_stats):
     if win == True:
         with_df[hero_id_map, with_pb_ids_map] += 1
         against_df[hero_id_map, against_pb_ids_map] += 1
-    else:
-        with_df[hero_id_map, with_pb_ids_map] -= 1
-        against_df[hero_id_map, against_pb_ids_map] -= 1
     
     return with_df, against_df
     
-def get_hero_stats():
-    hero_stats_path = r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\DotA2DraftAssistant\hero_stats\hero_stats.json'
+def get_hero_stats(hero_stats_path):
     hero_stats = pd.DataFrame(make_dict_from_path(hero_stats_path))
     (x, y) = hero_stats.shape
     hero_stats['idx_map'] = np.arange(0, x, 1)
     return hero_stats
 
-def get_player_wr():
-    player_wr_path = r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\player_37571649_heroes.json'
+def get_player_wr(player_wr_path):
     player_wr = make_dict_from_path(player_wr_path)
     player_wr = pd.DataFrame(player_wr)
     return player_wr
 
-# client.get_player_heroes(p_id)
-hero_stats = get_hero_stats()
+def main(match_path):
+    hero_stats_path = r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\DotA2DraftAssistant\hero_stats\hero_stats.json'
+    hero_stats = get_hero_stats(hero_stats_path)
+
+    player_wr_path = r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\player_37571649_heroes.json'
+    player_wr = get_player_wr(player_wr_path)
+
+    os.chdir(match_path)
+
+    files = os.listdir()
+
+    (x, y) = hero_stats.shape
+
+    heroes_with = np.zeros((x, x))
+    heroes_against = np.zeros((x, x))
+
+    for match_history in files:
+        match_data = make_dict_from_path(match_path + r"\\" + match_history)
+        pb, win, words, players = parse_match(match_data, p_id)
+        heroes_with, heroes_against = add_to_df(heroes_with, heroes_against, pb, win, hero_stats)
+
+    heroes_with /= len(files)
+    heroes_against /= len(files)
+
+    print(np.unique(heroes_with), np.unique(heroes_against))
 
 match_path = r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\DotA2DraftAssistant\matches'
-
-os.chdir(match_path)
-
-files = os.listdir()
-
-(x, y) = hero_stats.shape
-
-with_df = np.zeros((x, x))
-against_df = np.zeros((x, x))
-
-for match_history in files:
-    match_data = make_dict_from_path(match_path + r"\\" + match_history)
-    pb, win, words, players = parse_match(match_data, p_id)
-    with_df, against_df = add_to_df(with_df, against_df, pb, win, hero_stats)
-
-print(with_df)
-
-    
-
-# against_df = pd.DataFrame(against_df, columns = np.array(hero_stats['id']), index = np.array(hero_stats['id']))
-with_df = pd.DataFrame(with_df, columns = np.array(hero_stats['id']), index = np.array(hero_stats['id']))
-
-with_df.to_csv(r'C:\Users\nikhi\Documents\Clemson\CUHackit\2022\DotA2DraftAssistant\test.csv')
+main(match_path)
